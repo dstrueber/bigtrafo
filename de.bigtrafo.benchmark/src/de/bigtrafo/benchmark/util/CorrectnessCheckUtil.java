bb4e05635d26dda6917ab64f1ea3113e8b14461c
strueber <strueber@mathematik.uni-marburg.de>
2016-06-12 17:06:23 +0200

    Remove dependency to EMFCompare (textual diff is currently sufficient)

--- de.bigtrafo.benchmark/src/de/bigtrafo/benchmark/util/CorrectnessCheckUtil.java
+++ de.bigtrafo.benchmark/src/de/bigtrafo/benchmark/util/CorrectnessCheckUtil.java
@@ -1,29 +1,19 @@
 package de.bigtrafo.benchmark.util;
 
 import java.io.IOException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-import org.eclipse.emf.compare.diff.DefaultDiffEngine;
-import org.eclipse.emf.compare.diff.DiffBuilder;
-import org.eclipse.emf.compare.diff.FeatureFilter;
-import org.eclipse.emf.compare.diff.IDiffEngine;
-import org.eclipse.emf.compare.diff.IDiffProcessor;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.henshin.model.resource.HenshinResourceSet;
-
-import com.google.common.base.Charsets;
-import com.google.common.io.Resources;
 
 public class CorrectnessCheckUtil {
 	
 	/**
 	 * Checks if the input files are identical. <br>
-	 * TODO: Implement a more intelligent, semantic way to compare the files.
+	 * TODO: Implement a more intelligent, semantic way to compare the files (maybe using EMFCompare).
 	 * @param filePath1
 	 * @param filePath2
 	 */
@@ -39,81 +29,7 @@ public class CorrectnessCheckUtil {
 
 	}
 
-	// TODO: Get the following EMF compare running (currently finds differences
-	// in identical files)
-	//
-	// public static void compare(String filePath1, String filePath2) {
-	// URI uri1 = URI.createFileURI(filePath1);
-	// HenshinResourceSet resourceSet1 = new HenshinResourceSet();
-	// registerPackages(resourceSet1);
-	// resourceSet1.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*",
-	// new XMIResourceFactoryImpl());
-	// resourceSet1.getResource(uri1, true);
-	// EcoreUtil.resolveAll(resourceSet1);
-	//
-	// URI uri2 = URI.createFileURI(filePath2);
-	// HenshinResourceSet resourceSet2 = new HenshinResourceSet() ;
-	// registerPackages(resourceSet2);
-	// resourceSet2.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*",
-	// new XMIResourceFactoryImpl());
-	// resourceSet2.getResource(uri2, true);
-	// EcoreUtil.resolveAll(resourceSet2);
-	//
-	// // Configure EMF Compare
-	// IEObjectMatcher matcher =
-	// DefaultMatchEngine.createDefaultEObjectMatcher(UseIdentifiers.NEVER);
-	// IComparisonFactory comparisonFactory = new DefaultComparisonFactory(new
-	// DefaultEqualityHelperFactory());
-	//
-	// IDiffEngine diffEngine = createIgnoreOrderDiffEngine();
-	//
-	// IMatchEngine.Factory matchEngineFactory = new
-	// MatchEngineFactoryImpl(matcher, comparisonFactory);
-	// matchEngineFactory.setRanking(20);
-	// IMatchEngine.Factory.Registry matchEngineRegistry = new
-	// MatchEngineFactoryRegistryImpl();
-	// matchEngineRegistry.add(matchEngineFactory);
-	//
-	// EMFCompare comparator = EMFCompare.builder()
-	// .setMatchEngineFactoryRegistry(matchEngineRegistry).setDiffEngine(diffEngine).build();
-	//
-	// IComparisonScope scope = new DefaultComparisonScope(resourceSet1,
-	// resourceSet2, null);
-	// Comparison comparison = comparator.compare(scope);
-	//
-	// List<Diff> differences = comparison.getDifferences();
-	// System.out.println("Compared " + filePath1 + " vs. " + filePath2 + ": " +
-	// differences.size() + " differences.");
-	// for(Diff d: differences)
-	// {
-	// System.err.println("d.getKind(): "+d.getKind());
-	// System.err.println("d.getMatch(): " + d.getMatch());
-	// System.err.println("State: " + d.getState());
-	// }
-	//
-	// }
 
-	private static IDiffEngine createIgnoreOrderDiffEngine() {
-		IDiffProcessor diffProcessor = new DiffBuilder();
-		IDiffEngine diffEngine = new DefaultDiffEngine(diffProcessor) {
-			@Override
-			protected FeatureFilter createFeatureFilter() {
-				return new FeatureFilter() {
-					@Override
-					public boolean checkForOrderingChanges(EStructuralFeature feature) {
-						return false;
-					}
-				};
-			}
-		};
-		return diffEngine;
-	}
-
-	private static void registerPackages(HenshinResourceSet resourceSet1) {
-		resourceSet1.registerDynamicEPackages("ocl/Pivot.ecore");
-		resourceSet1.registerDynamicEPackages("ocl/GraphConstraint.ecore");
-
-	}
 
 	public static boolean performCorrectnessCheck(String resultPath, String referencePath, String fileExtension, RuntimeBenchmarkReport report) {
 		String resultFile = resultPath + "/" + findFile(resultPath, fileExtension);
